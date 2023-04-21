a6f2853832b570fc9d06b944a085908392e9b6ec
rbakijanovas <rolandasb94@gmail.com>
2023-04-21 07:17:00 +0300

    table

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var inputFName = document.getElementById("inputFName");
+var inputLName = document.getElementById("inputLName");
+var inputAge = document.getElementById("inputAge");
